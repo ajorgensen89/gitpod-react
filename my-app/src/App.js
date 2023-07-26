e0b0248264c62c9e49630d297872b8690e47d16a
ajorgensen89 <ajorgensen89@gmail.com>
2023-07-26 13:27:58 +0000

    Create a class and use this and state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import FunctionalGreeting from './components/FunctionalGreeting';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you" 
       name="Ava" cat="Bear" />
+      <StatefulGreeting greeting="working prop" dude="dude" />
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,34 @@
+// Not needed in new React but better to use for use with old.
+import React from "react";
+
+// do not return. Render.
+class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        //to call React.Components. Pass props so super pass props too.
+        super(props);
+        //access and manipulate {properties}
+        this.state = {
+            // Change intro on button click
+            introduction: "Hello",
+            buttonText: "Exit",
+
+        };
+    }
+
+
+    render() {
+        // Add {} for multiple lines of code
+        return (
+            <div>
+                {this.state.introduction}
+            <h1> Stateful {this.props.greeting},
+            {this.props.dude} </h1>
+            <button>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+
+}
+
+export default StatefulGreeting;
