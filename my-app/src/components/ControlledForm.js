56e6768477a2917dab1e721d2783a36ca60e7184
ajorgensen89 <ajorgensen89@gmail.com>
2023-07-31 16:59:40 +0000

    submit data for use in API

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -6,6 +6,8 @@ export class ControlledForm extends Component {
         super(props)
         this.state = {
             name: '',
+            category: 'website',
+            comments: '',
 
         }
     }
@@ -26,28 +28,46 @@ export class ControlledForm extends Component {
         })
     }
 
+    handleCategoryChange = (event) => {
+        this.setState({
+            category: event.target.value
+            
+        })
+    }
+
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+            
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
+
     render() {
         return (
           <div>
             <h2>Please fill out form:</h2>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 
                 <div>
                 <label htmlFor="id-name" >Your Name:</label>
                 <input value={this.state.name} onChange={this.handleNameChange} id="id-name" type="text" name="name" />
-                <input type="submit" value="Submit" />
                 </div>
                 <div>
                 <label htmlFor="id-category">Queries:</label>
-                <select id="id-category" name="category">
-                    <option value="website"></option>
-                    <option value="order"></option>
-                    <option value="general"></option>
+                <select value={this.state.category} onChange={this.handleCategoryChange} id="id-category" name="category">
+                    <option value="website">Website Issue</option>
+                    <option value="order">Order Issue</option>
+                    <option value="general">General Issue</option>
                 </select>
                 </div>
                 <div>
                     <label htmlFor="id-comments">Comments:</label>
-                    <textarea id="id-comments" name="comments" />
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange} id="id-comments" name="comments" />
                 </div>
                 <input type="submit" value="Submit" />
                 
