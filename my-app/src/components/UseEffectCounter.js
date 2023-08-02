9bb599205c29c8c6ff3c9b7ad67692398930b7ba
ajorgensen89 <ajorgensen89@gmail.com>
2023-08-02 12:17:46 +0000

    update one useEffect hook specify, can do more than one

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -4,8 +4,17 @@ import React, {useState, useEffect} from 'react'
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
     useEffect(() => {
+        console.log('count effect')
         document.title = count;
-    });
+    // Only runs when the 'count' chnages. 
+    }, [count]);
+
+    const [count2, setCount2] = useState(0);
+    useEffect(() => {
+        console.log('count2')
+        document.title = count2;
+    // Only runs when the 'count2' chnages. 
+    }, [count2]);
 
     return (
         <div>
@@ -14,6 +23,11 @@ function UseEffectCounter() {
                     Increment Count
                 </button>
                 <div>{count}</div>
+                <button onClick= {
+                () => setCount2(count2 => count2 + 1) }>
+                    Increment Count2
+                </button>
+                <div>{count2}</div>    
         </div>
     )
 }
